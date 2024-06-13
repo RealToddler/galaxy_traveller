54cf523422666fcd05d1b89abc92c9590e4fb189
noeblp <149522348+noeblp@users.noreply.github.com>
2024-06-13 13:59:46 +0200

    lvl3 steph
    lvl3

--- Assets/Scripts/Network/Loading.cs
+++ Assets/Scripts/Network/Loading.cs
@@ -31,6 +31,6 @@ public class Loading : MonoBehaviourPunCallbacks
     
     public override void OnCreatedRoom()
     {
-        PhotonNetwork.LoadLevel("Lvl2");
+        PhotonNetwork.LoadLevel("Lvl3");
     }
 }
