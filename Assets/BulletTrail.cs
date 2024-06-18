9b7fe3e34645e7e2e8ad469df774c2776ff2d094
Joblnc <blancjohan17@gmail.com>
2024-06-18 17:04:56 +0200

    projectile niquel

--- Assets/BulletTrail.cs
+++ Assets/BulletTrail.cs
@@ -8,17 +8,21 @@ public class BulletTrail : MonoBehaviour
     private LineRenderer lineRenderer;
     private Vector3[] positions = new Vector3[2];
     private float timeOfInstantiation;
+    private Rigidbody rigidbody;
+    private Vector3 _spawnpoint;
 
     void Start()
     {
+        _spawnpoint=transform.position; 
         timeOfInstantiation=Time.time;
         lineRenderer = gameObject.AddComponent<LineRenderer>();
         lineRenderer.startWidth = 0.1f;
         lineRenderer.endWidth = 0.1f;
         lineRenderer.positionCount = 2;
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.startColor = Color.gray;
-        lineRenderer.endColor = Color.gray;
+        lineRenderer.startColor = Color.red;
+        lineRenderer.endColor = Color.yellow;
+        rigidbody=GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -27,7 +31,7 @@ public class BulletTrail : MonoBehaviour
         if (delay>0.02)
         {
             positions[0] = transform.position;
-            positions[1] = transform.position + transform.up * 2;
+            positions[1] = _spawnpoint;
             lineRenderer.SetPositions(positions);
         }
         
