a0f92428aac75c241bd7b31e296813b6e6f5c1c2
Aubin <33763296+aubinthome@users.noreply.github.com>
2024-06-18 17:36:20 +0200

    Bullet modif

--- Assets/BulletTrail.cs
+++ Assets/BulletTrail.cs
@@ -8,32 +8,29 @@ public class BulletTrail : MonoBehaviour
     private LineRenderer lineRenderer;
     private Vector3[] positions = new Vector3[2];
     private float timeOfInstantiation;
-    private Rigidbody rigidbody;
     private Vector3 _spawnpoint;
 
     void Start()
     {
-        _spawnpoint=transform.position; 
-        timeOfInstantiation=Time.time;
+        _spawnpoint = transform.position; 
+        timeOfInstantiation = Time.time;
         lineRenderer = gameObject.AddComponent<LineRenderer>();
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0;
         lineRenderer.positionCount = 2;
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         lineRenderer.startColor = Color.red;
-        lineRenderer.endColor = Color.yellow;
-        rigidbody=GetComponent<Rigidbody>();
+        lineRenderer.endColor = new Color(255, 255, 0, 0);
     }
 
     void Update()
     {
-        float delay=Time.time - timeOfInstantiation;
+        float delay = Time.time - timeOfInstantiation;
         if (delay>0.02)
         {
             positions[0] = transform.position;
             positions[1] = _spawnpoint;
             lineRenderer.SetPositions(positions);
         }
-        
     }
 }
